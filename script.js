e727cad7338af3f282c292c6a67da8b0b5169da6
harshith-1604 <harshithchavva1604@gmail.com>
2024-08-31 18:21:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+const navbarLinks=document.querySelectorAll('.navbar a');
+navbarLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const sectionId = link.getAttribute('href');
+    const section = document.querySelector(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+});
